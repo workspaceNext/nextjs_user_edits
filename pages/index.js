dbda90db94fc311f6d05640b976cd976e299e774
Gurjot <87029898+gsandhu-vercel@users.noreply.github.com>
2021-10-28 19:15:57 -0500

    testfixed
    test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to the User Test</a>
+          Welcome to the User Test
         </h1>
 
         <p className={styles.description}>
